4e4d854c665adde65510898ddfadb1ada44f8b90
Brian Guerrero <13bguerrero@gmail.com>
2016-09-19 14:08:05 -0500

    Created some base code to start using the Collections object.

--- CormorantProject/src/Collections.java
+++ CormorantProject/src/Collections.java
@@ -1,8 +1,52 @@
+import java.util.Date;
 import java.util.List;
 
 public class Collections {
 	private List<Person> personCollection;
 	private List<Interaction> interactionCollection;
+		
+	//Person Collection Methods
+	public void addPerson(Person person){
+		personCollection.add(person);
+	}
 	
 	
+	public void deletePerson(Person person){
+		personCollection.remove(person);
+	}
+	
+	public void editPerson(Person person, String name, String gender, List<String> culture, String occupation, String notes){
+		person = new Person(name, gender, culture, occupation, notes);
+	}
+	
+	//Interaction Collection Methods
+	public void addInteraction(Interaction interaction){
+		interactionCollection.add(interaction);
+	}
+	
+	public void deleteInteraction(Interaction interaction){
+		interactionCollection.remove(interaction);
+	}
+	
+	public void editInteraction(Interaction interaction, List<Person> people, String location, Date date, List<String> interactionType,
+			List<String> citation, String notes){
+		interaction = new Interaction(people, location, date, interactionType, citation, notes);
+	}
+	
+	//CSV Collection Methods
+	public List<Person> loadDataPerson(String fileName){
+		return personCollection; //Temporary return object.
+	}
+	
+	public List<Interaction> loadDataInteraction(String fileName){
+		return interactionCollection; //Temporary return object.
+	}
+	
+	public void saveDataPerson(String fileName){
+		//Saves people to people CSV File.
+	}
+	
+	public void saveDataInteraction(String fileName){
+		//Saves interactions to interaction CSV File.
+	}
 }
