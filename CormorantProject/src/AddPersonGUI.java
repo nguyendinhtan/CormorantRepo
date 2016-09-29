ffaf504d5056cbdb7166a028e4b7e6fa149ecafe
jhaeme15 <jaredhaeme15@augustana.edu>
2016-09-29 00:12:04 -0500

    Updated GUI to look better and added event handlers for switching between windows

--- CormorantProject/src/AddPersonGUI.java
+++ CormorantProject/src/AddPersonGUI.java
@@ -9,54 +9,83 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.collections.*;
-public class AddPersonGUI extends Application{
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+public class AddPersonGUI extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
 
 	}
+
 	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Home Screen");
+		primaryStage.setTitle("Insert Person");
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(10);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
+		Label nameLabel = new Label("Name:");
+		grid.add(nameLabel, 0, 0);
 		TextField nameTextField = new TextField();
-	       grid.add(nameTextField, 1, 0);
-		ComboBox gender = new ComboBox(); 
-		gender.getItems().addAll(
-				"Male",
-				"Female",
-				"Unknown"
-		);
+		grid.add(nameTextField, 1, 0);
+		Label genderLabel = new Label("Gender:");
+		grid.add(genderLabel, 0, 1);
+		ComboBox gender=new ComboBox();
+		gender.getItems().addAll("Male", "Female", "Unknown");
+		gender.setMinSize(300, 10);
+		System.setProperty("glass.accessible.force", "false");//Fixes bug of combobox crashing when running on certain computers
 		grid.add(gender, 1, 1);
-		
-	       TextField cultureTextField = new TextField();
-	       grid.add(cultureTextField, 1, 2);
-	       TextField occupationTextField = new TextField();
-	       grid.add(occupationTextField, 1, 3);
-	       TextField notesTextField = new TextField();
-	       grid.add(notesTextField, 1, 3);
+		Label cultureLabel = new Label("Culture Identity:");
+		grid.add(cultureLabel, 0, 2);
+		TextField cultureTextField = new TextField();
+		grid.add(cultureTextField, 1, 2);
+		Label occupationLabel = new Label("Occupation:");
+		grid.add(occupationLabel, 0, 3);
+		TextField occupationTextField = new TextField();
+		grid.add(occupationTextField, 1, 3);
+		Label notesLabel = new Label("Notes:");
+		grid.add(notesLabel, 0, 4);
+		TextField notesTextField = new TextField();
+		notesTextField.setMinSize(300, 100);
+		grid.add(notesTextField, 1, 4);
 		Button btnAddPerson = new Button("Add Person");
 		btnAddPerson.setTextFill(Color.BLACK);
 		btnAddPerson.setTextFill(Color.WHITE);
 		btnAddPerson.setStyle("-fx-base: #FF0000");
-		HBox hbBtn = new HBox(10);
-		hbBtn.setAlignment(Pos.CENTER_RIGHT);
-		hbBtn.getChildren().add(btnAddPerson);
+		grid.add(btnAddPerson, 0, 6);
 
-		
 		Button btnBack = new Button("Back");
 		btnBack.setTextFill(Color.WHITE);
 		btnBack.setStyle("-fx-base: #FF0000");
+		HBox hbBtn=new HBox();
 		hbBtn.getChildren().add(btnBack);
-		grid.add(hbBtn, 1, 4);
-
-		Scene scene = new Scene(grid, 1000, 1000);
+		hbBtn.setAlignment(Pos.CENTER_RIGHT);
+		grid.add(hbBtn, 1, 6);
+		Scene scene = new Scene(grid, 500, 400);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+		btnAddPerson.setOnAction(new EventHandler<ActionEvent>() {
+       	 
+            @Override
+            public void handle(ActionEvent e) {
+                nameTextField.clear();
+                cultureTextField.clear();
+                occupationTextField.clear();
+                notesTextField.clear();
+            }
+        });
+		btnBack.setOnAction(new EventHandler<ActionEvent>() {
+	       	 
+            @Override
+            public void handle(ActionEvent e) {
+            	HomeGUI Homegui=new HomeGUI();
+            	Homegui.start(primaryStage);
+            }
+        });
 	}
 
 }
+
 
---
+++ CormorantProject/src/HomeGUI.java
@@ -0,0 +1,87 @@
+import javafx.application.*;
+import javafx.event.*;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.*;
+public class HomeGUI extends Application{
+	public static void main(String[] args){
+		launch(args);
+	}
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Home Screen");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        TextField searchTextField = new TextField();
+        searchTextField.setMinSize(400, 10);
+        HBox hbTopRow = new HBox();
+        hbTopRow.setAlignment(Pos.CENTER_RIGHT);
+        hbTopRow.getChildren().add(searchTextField);
+
+        Button btnSearch = new Button("Search");
+        btnSearch.setTextFill(Color.WHITE);
+        btnSearch.setStyle("-fx-base: #FF0000"); 
+        hbTopRow.getChildren().add(btnSearch);
+        grid.add(hbTopRow, 1, 0);
+        
+        
+        
+        Button btnInsertPerson = new Button("Insert Person");
+        btnInsertPerson.setTextFill(Color.BLACK); 
+        HBox hbBtnBottomRow = new HBox(10);
+        hbBtnBottomRow.getChildren().add(btnInsertPerson);
+        btnInsertPerson.setOnAction(new EventHandler<ActionEvent>() {
+       	 
+            @Override
+            public void handle(ActionEvent e) {
+            	AddPersonGUI gui=new AddPersonGUI();
+            	gui.start(primaryStage);
+            }
+        });
+        
+        Button btnInsertInteraction = new Button("Insert Interaction");
+        btnInsertPerson.setTextFill(Color.BLACK); 
+        hbBtnBottomRow.getChildren().add(btnInsertInteraction);
+        grid.add(hbBtnBottomRow, 1, 1);
+        btnInsertInteraction.setOnAction(new EventHandler<ActionEvent>() {
+          	 
+            @Override
+            public void handle(ActionEvent e) {
+            	AddInteractionGUI gui=new AddInteractionGUI();
+            	gui.start(primaryStage);
+            }
+        });
+        Button btnEditVocab = new Button("Edit Controlled Vocabulary");
+        btnEditVocab.setTextFill(Color.BLACK);
+        hbBtnBottomRow.getChildren().add(btnEditVocab);
+        
+        
+        btnEditVocab.setOnAction(new EventHandler<ActionEvent>() {
+          	 
+            @Override
+            public void handle(ActionEvent e) {
+            	EditVocabGUI gui=new EditVocabGUI();
+            	gui.start(primaryStage);
+            }
+        });
+        Scene scene = new Scene(grid, 700, 300);
+        primaryStage.setScene(scene);	
+        primaryStage.show();
+    }
+    
+}
+	
