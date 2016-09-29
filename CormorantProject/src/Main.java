08a14b66f59db102d265e3b04699efd42b73f91c
Brian Guerrero <13bguerrero@gmail.com>
2016-09-29 00:14:00 -0500

    Created the SearchUtil class

--- CormorantProject/src/Main.java
+++ CormorantProject/src/Main.java
@@ -1,3 +1,13 @@
+/**
+ * The Main class will be the entry point of our application.
+ * 
+ * @author Jared Haeme
+ * @author Eric Sheidler
+ * @author Tan Nguyen
+ * @author Brian Guerrero
+ */
+
+
 public class Main {
 
 	public static void main(String[] args) {
---
+++ CormorantProject/src/SearchUtil.java
@@ -0,0 +1,43 @@
+import java.awt.List;
+
+/**
+ * This class contains all methods for searching through collections, persons, and interactions.
+ *
+ */
+public class SearchUtil {
+
+	/**
+	 * 
+	 * @param query the search query
+	 * @return serachResultsList the list of search results
+	 */
+	public static List search(String query){
+		
+		List searchResultsList = null;
+		return searchResultsList;
+	}
+	
+	/**
+	 * 
+	 * @param query the search query
+	 * @return searchInteractionsResultsList the list of search interaction results 
+	 */
+	public static List searchInteractions(String query){
+		List searchInteractionsResultsList = null;
+		return searchInteractionsResultsList;
+	}
+	
+	/**
+	 * 
+	 * @param query the search query
+	 * @return searchPeopleResultsList the list of search people results
+	 */
+	public static List searchPeople(String query){
+		List searchPeopleResultsList = null;
+		return searchPeopleResultsList;
+	}
+	
+	
+	
+	
+}
