ebf72177aac874da0ce362cd30fa3e4194689f4f
Brian Guerrero <13bguerrero@gmail.com>
2016-09-30 02:55:48 -0500

    Refactored Home GUI to be more readable.

--- CormorantProject/src/HomeGUI.java
+++ CormorantProject/src/HomeGUI.java
@@ -12,37 +12,46 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class HomeGUI extends Application{
+	
 	public static void main(String[] args){
 		launch(args);
 	}
+	
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Home Screen");
+        //GUI Variables
         GridPane grid = new GridPane();
+        Scene scene = new Scene(grid, 700, 300);
+        TextField searchTextField = new TextField();
+        Button searchButton = new Button("Search");
+        Button insertPersonButton = new Button("Insert Person");
+        Button insertInteractionButton = new Button("Insert Interaction");
+        Button editVocabButton = new Button("Edit Controlled Vocabulary");
+        HBox topRowBox = new HBox();
+        HBox bottomButtonRowBox = new HBox(10);
+        
+        //Grid Methods
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
-
-        TextField searchTextField = new TextField();
+        grid.add(topRowBox, 1, 0);
+        grid.add(bottomButtonRowBox, 1, 1);
+        
+        //Search Text Field Methods
         searchTextField.setMinSize(400, 10);
-        HBox hbTopRow = new HBox();
-        hbTopRow.setAlignment(Pos.CENTER_RIGHT);
-        hbTopRow.getChildren().add(searchTextField);
-
-        Button btnSearch = new Button("Search");
-        btnSearch.setTextFill(Color.WHITE);
-        btnSearch.setStyle("-fx-base: #FF0000"); 
-        hbTopRow.getChildren().add(btnSearch);
-        grid.add(hbTopRow, 1, 0);
         
+        //Search Button Methods
+        searchButton.setTextFill(Color.WHITE);
+        searchButton.setStyle("-fx-base: #FF0000"); 
         
-        
-        Button btnInsertPerson = new Button("Insert Person");
-        btnInsertPerson.setTextFill(Color.BLACK); 
-        HBox hbBtnBottomRow = new HBox(10);
-        hbBtnBottomRow.getChildren().add(btnInsertPerson);
-        btnInsertPerson.setOnAction(new EventHandler<ActionEvent>() {
-       	 
+        //Top Row Box Methods
+        topRowBox.setAlignment(Pos.CENTER_RIGHT);
+        topRowBox.getChildren().add(searchTextField);
+        topRowBox.getChildren().add(searchButton);
+ 
+        //Insert Person Button Methods
+        insertPersonButton.setTextFill(Color.BLACK);
+        insertPersonButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
             	AddPersonGUI gui=new AddPersonGUI();
@@ -50,32 +59,32 @@ public class HomeGUI extends Application{
             }
         });
         
-        Button btnInsertInteraction = new Button("Insert Interaction");
-        btnInsertPerson.setTextFill(Color.BLACK); 
-        hbBtnBottomRow.getChildren().add(btnInsertInteraction);
-        grid.add(hbBtnBottomRow, 1, 1);
-        btnInsertInteraction.setOnAction(new EventHandler<ActionEvent>() {
-          	 
+        //Insert Interaction Button Methods
+        insertInteractionButton.setOnAction(new EventHandler<ActionEvent>() {  	 
             @Override
             public void handle(ActionEvent e) {
             	AddInteractionGUI gui=new AddInteractionGUI();
             	gui.start(primaryStage);
             }
         });
-        Button btnEditVocab = new Button("Edit Controlled Vocabulary");
-        btnEditVocab.setTextFill(Color.BLACK);
-        hbBtnBottomRow.getChildren().add(btnEditVocab);
-        
-        
-        btnEditVocab.setOnAction(new EventHandler<ActionEvent>() {
-          	 
+              
+        //Edit Vocab Button Methods
+        editVocabButton.setTextFill(Color.BLACK);
+        editVocabButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
             	EditVocabGUI gui=new EditVocabGUI();
             	gui.start(primaryStage);
             }
         });
-        Scene scene = new Scene(grid, 700, 300);
+        
+        //Bottom Button Row Box
+        bottomButtonRowBox.getChildren().add(insertPersonButton);
+        bottomButtonRowBox.getChildren().add(insertInteractionButton);
+        bottomButtonRowBox.getChildren().add(editVocabButton);
+  
+        //Primary Stage Methods
+        primaryStage.setTitle("Home Screen");
         primaryStage.setScene(scene);	
         primaryStage.show();
     }
