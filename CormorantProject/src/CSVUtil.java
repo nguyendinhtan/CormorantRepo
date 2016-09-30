5f70223170a6f8282f33a4f2c7569f33f1af0a09
Tan Nguyen <nguyendinhtan96@gmail.com>
2016-09-30 01:14:12 -0500

    Included addPerson, loadPerson and savePerson Have to work on loadInteractions and saveInteractions

--- CormorantProject/src/CSVUtil.java
+++ CormorantProject/src/CSVUtil.java
@@ -1,17 +1,19 @@
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.TreeMap;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
-public class CSVUtil {
-	private Map<Integer,Person> personMap;
+public class CSVUtil{
+	
+	private Map<Integer, Person> personMap;
 	private List<Interaction> interactions;
 
 	public CSVUtil() {
@@ -19,18 +21,67 @@ public class CSVUtil {
 		interactions =  new ArrayList<>();
 	}
 
-	public void loadWatchers(String fileName) throws IOException {
+	public void loadPerson(String fileName) throws IOException {
 		CSVReader reader = new CSVReader(new FileReader(fileName));
-		Scanner inputStream = new Scanner(fileName);
+
 		List<String[]> myRows = reader.readAll();
-		while (inputStream.hasNext())	{
-			String data=inputStream.next();
-		}
-		inputStream.close();
 		String[] headerRow = myRows.remove(0); // remove header row
-/*
+
 		for (String[] row : myRows) {
-			addWatcher(new BirdWatcher(row));
-		}*/
+			addPerson(new Person(row));
+		}
+// TODO: Delete DEBUG CODE later 
+//		for (Integer key : watcherMap.keySet()) {
+//			System.out.println("key: " + key + " value: " + watcherMap.get(key));
+//		}
 	}
+	public void addPerson(Person person) {
+		personMap.put(person.getID(), person);
+	}
+
+	public void savePerson(String fileName) throws IOException {
+		CSVWriter writer = new CSVWriter(new FileWriter(fileName));
+		for (Person person : personMap.values()) {
+		     writer.writeNext(person.toCSVRowArray());		     
+		}
+		 writer.close();	   
+	}
+/*
+	public void loadInteractions(String fileName) throws IOException {		
+		CSVReader reader = new CSVReader(new FileReader(fileName));		
+		List<String[]> myRows = reader.readAll();
+
+		// ,Old Main,0:3:7,Pelican=3:Green Heron=1
+		for (String[] row : myRows) {
+			String date = row[0];
+			String location= row[1];
+			String idListText=row[2];
+			String obsListText=row[3];
+			String[] idArray = idListText.split(":");
+			List<BirdWatcher> watchersThisTrip = new ArrayList<BirdWatcher>();
+
+			for (String idStr : idArray) {
+				int id = Integer.parseInt(idStr);
+				BirdWatcher watcher = watcherMap.get(id);
+				watchersThisTrip.add(watcher); 
+			}
+			
+			List<BirdObservation> birdObsThisTrip = new ArrayList<>(); 
+			String[] obsListArray = obsListText.split(":");
+			for (String obsText : obsListArray) {
+				String[] pair = obsText.split("=");
+				String birdName = pair[0];
+				int birdCount = Integer.parseInt(pair[1]);
+				birdObsThisTrip.add(new BirdObservation(birdName, birdCount));
+			}
+			BirdingTrip trip = new BirdingTrip(location, watchersThisTrip, date, birdObsThisTrip);
+			trips.add(trip);
+			
+		}		
+	}
+
+	public void saveTrips(String fileName) {
+		
+	}
+*/
 }
