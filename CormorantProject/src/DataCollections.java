aed314b1da2b8c74125f92c130f937a61c235548
jhaeme15 <jaredhaeme15@augustana.edu>
2016-10-02 23:20:12 -0500

    Added comments

--- CormorantProject/src/DataCollections.java
+++ CormorantProject/src/DataCollections.java
@@ -113,6 +113,7 @@ public class DataCollections {
 	
 	public int checkForInteractionDuplicates(Interaction interaction){
 		for (int i=0; i<interactionCollection.size(); i++){
+			//Checks for nullpointerexception when comparing null values with equals from duplicates
 			try{
 			if (interactionCollection.get(i).getPeople1().equals(interaction.getPeople1())&&interactionCollection.get(i).getPeople2().equals(interaction.getPeople2()) &&interactionCollection.get(i).getLocation().equals(interaction.getLocation())&& interactionCollection.get(i).getDate().equals(interaction.getDate())&&interactionCollection.get(i).getInteractionType().equals(interaction.getInteractionType())){
 					return i;
