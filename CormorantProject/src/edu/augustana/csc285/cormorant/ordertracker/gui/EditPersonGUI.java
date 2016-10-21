763cd04be429dbfe168358cacf47976b146af87c
Brian Guerrero <13bguerrero@gmail.com>
2016-10-21 13:58:15 -0500

    Removed unused imports

--- CormorantProject/src/edu/augustana/csc285/cormorant/ordertracker/gui/EditPersonGUI.java
+++ CormorantProject/src/edu/augustana/csc285/cormorant/ordertracker/gui/EditPersonGUI.java
@@ -14,8 +14,6 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
