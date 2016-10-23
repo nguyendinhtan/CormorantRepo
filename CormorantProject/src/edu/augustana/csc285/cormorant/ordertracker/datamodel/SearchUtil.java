38436922c6fabb4e0bb3a97bec8cf9350029c6ec
Brian Guerrero <13bguerrero@gmail.com>
2016-10-23 15:50:02 -0500

    Refactored for loops to be more readable.

--- CormorantProject/src/edu/augustana/csc285/cormorant/ordertracker/datamodel/SearchUtil.java
+++ CormorantProject/src/edu/augustana/csc285/cormorant/ordertracker/datamodel/SearchUtil.java
@@ -20,11 +20,11 @@ public class SearchUtil {
 
 	public static List<Interaction> searchInteractions(String search) {
 		List<Interaction> interactionResults = new ArrayList<>();
-		for (int i = 0; i < DataCollections.getInteractionCollection().size(); i++) {
-			if (DataCollections.getInteractionCollection().get(i).contains(search) == 1) {
-				interactionResults.add(0, DataCollections.getInteractionCollection().get(i));
-			} else if (DataCollections.getInteractionCollection().get(i).contains(search) == 2) {
-				interactionResults.add(DataCollections.getInteractionCollection().get(i));
+		for (Interaction interaction : DataCollections.getInteractionCollection()) {
+			if (interaction.contains(search) == 1) {
+				interactionResults.add(0, interaction);
+			} else if (interaction.contains(search) == 2) {
+				interactionResults.add(interaction);
 			}
 		}
 		return interactionResults;
@@ -39,11 +39,11 @@ public class SearchUtil {
 
 	public static List<Person> searchPeople(String search) {
 		List<Person> personResults = new ArrayList<>();
-		for (int i = 0; i < DataCollections.getPersonCollection().size(); i++) {
-			if (DataCollections.getPersonCollection().get(i).contains(search) == 1) {
-				personResults.add(0, DataCollections.getPersonCollection().get(i));
-			} else if (DataCollections.getPersonCollection().get(i).contains(search) == 2) {
-				personResults.add(DataCollections.getPersonCollection().get(i));
+		for (Person person : DataCollections.getPersonCollection()) {
+			if (person.contains(search) == 1) {
+				personResults.add(0, person);
+			} else if (person.contains(search) == 2) {
+				personResults.add(person);
 			}
 
 		}
